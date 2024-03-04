6a9d6bcde29ec7a68acbbb9bde45aed38778a62f
D4-80565-PramitSamanta <63332503+D4-80565-PramitSamanta@users.noreply.github.com>
2024-03-04 13:13:46 +0530

    changing path
    @RepositoryRestResource use to change url path
    path = "members"

--- src/main/java/com/datarest/repo/UserRepo.java
+++ src/main/java/com/datarest/repo/UserRepo.java
@@ -1,10 +1,11 @@
 package com.datarest.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 
 import com.datarest.entity.User;
-
+@RepositoryRestResource(path = "members")
 public interface UserRepo extends JpaRepository<User, Long> {
 	
 }
